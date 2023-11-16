6803a14ce86ca388ed9c5c994ee510a378fc046e
KRis20f <cristiangutierrez008@gmail.com>
2023-11-16 23:07:31 +0100

    Se Creo un archivo de C# ejecutable, con el proposito de ver si hay numeros pares e impares

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number;
+        Console.WriteLine("Ingresa el numero");
+        number = Convert.ToInt32(Console.ReadLine());
+        if (number%2 == 0) {
+            Console.WriteLine("Es par Chamo");
+        }
+
+        else {
+            Console.WriteLine("Es impar bro");
+        }
+    }
+}
